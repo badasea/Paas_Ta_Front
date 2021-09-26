0786af319046a0b2a2c3b9ca0464e2a81bdac8c6
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-09-26 20:08:00 +0900

    프론트엔드 스타트

--- paas/src/index.js
+++ paas/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App2 from './App';
 import reportWebVitals from './reportWebVitals';
+import Index from './Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
